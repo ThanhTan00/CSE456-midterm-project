4f60a2d6cf7cad37c9e998ba7d8e3c91403adf89
ThanhTan00 <tan.lenguyen.cit19@eiu.edu.vn>
2024-04-22 19:00:31 +0700

    Initial commit

---
+++ src/java/config/ConfigInfo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package config;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ConfigInfo {
+    private static String ctxRealPath="";
+
+    public static String getCtxRealPath() {
+        return ctxRealPath;
+    }
+
+    public static void setCtxRealPath(String ctxRealPath) {
+        ConfigInfo.ctxRealPath = ctxRealPath;
+    }
+}
---
+++ web/view/user/userHomePage.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : userHomePage
+    Created on : Apr 22, 2024, 6:27:50 PM
+    Author     : ADMIN
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello mr.Thao!</h1>
+    </body>
+</html>
